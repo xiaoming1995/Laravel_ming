e6e0b2a3e1fb29d777d0d92f168771ef0131e2ed
xiaoming1995 <460667926@qq.com>
2018-05-13 15:08:37 +0800

    laravel_ming
    记录自己第一个学习项目

---
+++ app/Http/Controllers/StaticPageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;use Illuminate\Support\Facades\DB;
+
+class StaticPageController extends Controller
+{
+    public function main(){
+    
+        return 'main';
+
+    }
+}
