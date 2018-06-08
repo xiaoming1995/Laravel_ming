6a5442b8444b2e62069f5277c6903d985305770b
vagrant <vagrant@homestead>
2018-06-08 02:31:53 +0000

    权限

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,7 +9,24 @@ use App\Models\User;
 
 
 class UsersController extends Controller
-{
+{   
+    public function __construct() 
+    { 
+        $this->middleware('auth',[
+            'except'=>['show','create','store','index']
+        ]);
+
+         $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+    public function index(){ 
+        $users  = User::paginate(10);
+        return view('users.index',compact('users'));
+    }
+
+
     public function create(){
     	return view('users.create');
     } 
@@ -37,6 +54,44 @@ class UsersController extends Controller
         return redirect()->route('users.show', [$user]);
 
     }
+
+    public function edit(User $user){ 
+        $this->authorize('update',$user);
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request){ 
+        $this->authorize('update',$user);
+
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'password'=>'required|confirmed|min:6',
+        ]);
+        $data = [];
+        $data['name'] = $request->name;
+
+        $data['password'] = !empty($request->password) ? bcrypt($request->password) : '';
+
+        $user->update($data);
+
+        // $user->update([
+        //     'name'=>$request->name,
+        //     'password'=>bcrypt($request->password),
+        // ]);
+
+        return redirect()->route('users.show',$user->id);
+
+    }
+
+
+    public function destroy(User $user){ 
+        
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
+
 
 
 }
